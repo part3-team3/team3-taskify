31d03284a3a05af923fbd0a44c569c151abe594f
JiminN2 <162524947+JiminN2@users.noreply.github.com>
2024-06-29 14:03:26 +0900

    Update DashboardMembersEdit.tsx

--- components/dashboard/DashboardMembersEdit.tsx
+++ components/dashboard/DashboardMembersEdit.tsx
@@ -5,7 +5,6 @@ const DashboardMembersEdit = () => {
       <div className="flex h-[404px] w-[620px] flex-col rounded-lg bg-white px-24 pt-25">
         <div className="flex justify-between">
           <div className="mb-30 text-xl font-bold">구성원</div>
-          {/* <div className="text-sm">{pages}페이지 중{currentPage}</div> */}
         </div>
 
         <p className="font-sm text-[1.125rem] text-[#9FA6B2]">이름</p>
