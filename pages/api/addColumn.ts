478ee976c09a27a33548e5e98e8bf606b51dad5b
emotigom <ahnsangkyoon@gmail.com>
2024-07-01 13:11:23 +0900

    Fix: 컬럼부분 token 처리

--- pages/api/addColumn.ts
+++ pages/api/addColumn.ts
@@ -1,9 +1,7 @@
 import axios from '@/lib/axios';
 
 export const addColumn = async (dashboardId: number) => {
-  const token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDAwMywidGVhbUlkIjoiNi0zIiwiaWF0IjoxNzE5NDgzMjA2LCJpc3MiOiJzcC10YXNraWZ5In0.j4rrDwXVyrJnrIZNFSOgSHzj6lz2S0qHjmBpgK-8FkM';
-
+  const token = process.env.NEXT_PUBLIC_TOKEN;
   const body = {
     title: 'empty',
     dashboardId: dashboardId, // dashboardId를 숫자형으로 변환해서 동적 연결
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import TodoModal from '@/components/TodoModal';
+import Column from '@/components/column/Column';
 
 export default function Home() {
   return (
     <>
       <TodoModal />
+      <Column />
     </>
   );
 }
