7cb22e42806011bf4d06c5a24ee9c814eff78dd1
emotigom <ahnsangkyoon@gmail.com>
2024-07-03 02:46:56 +0900

    Feat: 대시보드컬럼에서 컬럼카드로 전달하는 props 설정

--- pages/api/column/getCardList.ts
+++ pages/api/column/getCardList.ts
@@ -1,18 +1,14 @@
 import axios from '@/lib/axios';
 
-export const getCardList = async (cardSize: number, columnId: number) => {
+export const getCardList = async (columnId: number) => {
   const token = process.env.NEXT_PUBLIC_TOKEN;
-  const body = {
-    columnId: `${columnId}`,
-  };
-  const res = await axios.put(
-    `cards?size=${cardSize}&columnId=${columnId}`,
-    body,
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
+  // const body = {
+  //   columnId: `${columnId}`,
+  // };
+  const res = await axios.get(`cards?columnId=${columnId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
     },
-  );
+  });
   return res.data;
 };
