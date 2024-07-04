b9e0bf1a0ad35aeaf39b250aaf9201a82be3a064
emotigom <ahnsangkyoon@gmail.com>
2024-07-05 01:16:41 +0900

    Fix: API 사용안하는 코드 삭제

--- pages/api/column/getColumn.ts
+++ pages/api/column/getColumn.ts
@@ -1,14 +1,8 @@
 import axios from '@/lib/axios';
 
 export const getColumn = async (dashboardId: number) => {
-  // const token = process.env.NEXT_PUBLIC_TOKEN;
   const res = await axios.get(
     `columns?dashboardId=${dashboardId}`,
-    //   , {
-    //   headers: {
-    //     Authorization: `Bearer ${token}`,
-    //   },
-    // }
   );
   return res.data;
 };
