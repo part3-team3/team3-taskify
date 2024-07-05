bd8647fb706b4f3c41f2329964af78b33790339c
JiminN2 <162524947+JiminN2@users.noreply.github.com>
2024-07-06 04:17:35 +0900

    fix: sidebar 추가

--- pages/dashboard/[dashboardId]/edit/index.tsx
+++ pages/dashboard/[dashboardId]/edit/index.tsx
@@ -16,9 +16,7 @@ const HomePage = () => {
     if (!dashboardId || isNaN(dashboardId)) return;
     try {
       await axios.delete(`/dashboards/${dashboardId}`, {
-        params: {
-          dashboardId,
-        },
+        params: { dashboardId },
       });
       alert('대시보드가 삭제되었습니다.');
       router.push('/dashboards');
@@ -28,38 +26,42 @@ const HomePage = () => {
     }
   };
 
-  //돌아가기 버튼
   const handleBack = () => {
     router.push(`/dashboard/${dashboardId}`);
   };
 
   return (
-    <div className="min-h-screen bg-gray-100">
-      <NavBar />
-      <button
-        onClick={handleBack}
-        className="mt-15 flex items-center px-24 py-4"
-      >
-        <Image src={icArrowForward} width={20} height={20} alt="화살표" />
-        돌아가기
-      </button>
+    <div className="flex bg-gray-100">
       <div>
-        <div className="mt-15">
-          <DashboardNameEdit />
-        </div>
-        <div className="mt-15">
-          <DashboardMembersEdit />
-        </div>
-        <div className="mb-40 mt-15">
-          <DashboardInviteList />
-        </div>
-        <div className="px-24">
-          <button
-            onClick={handleDelete}
-            className="mb-56 h-[62px] w-[320px] gap-10 rounded-lg border border-[1px] border-gray-300 text-lg font-medium"
-          >
-            대시보드 삭제하기
-          </button>
+        <SideBar />
+      </div>
+      <div className="min-w-0 flex-1">
+        <NavBar />
+        <button
+          onClick={handleBack}
+          className="mt-15 flex items-center px-24 py-4"
+        >
+          <Image src={icArrowForward} width={20} height={20} alt="화살표" />
+          돌아가기
+        </button>
+        <div>
+          <div className="mt-15">
+            <DashboardNameEdit />
+          </div>
+          <div className="mt-15">
+            <DashboardMembersEdit />
+          </div>
+          <div className="mb-40 mt-15">
+            <DashboardInviteList />
+          </div>
+          <div className="px-24">
+            <button
+              onClick={handleDelete}
+              className="mb-56 h-[62px] w-[320px] gap-10 rounded-lg border border-[1px] border-gray-300 text-lg font-medium"
+            >
+              대시보드 삭제하기
+            </button>
+          </div>
         </div>
       </div>
     </div>
