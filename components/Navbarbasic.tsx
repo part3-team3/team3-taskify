1bf3fbf90134591caf5b35e9cbbda82694363eff
JiminN2 <162524947+JiminN2@users.noreply.github.com>
2024-07-06 00:08:09 +0900

    Feat:Navbarbasic/dropdown

--- components/Navbarbasic.tsx
+++ components/Navbarbasic.tsx
@@ -3,10 +3,13 @@ import instance from '@/lib/axios';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 
-import MembersImage from './EditDashboard/MembersImage';
-
-const NavBar = () => {
+const NavBarbasic = () => {
   const [nickname, setNickname] = useState('');
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false); // 드롭다운 상태
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
 
   useEffect(() => {
     const fetchNickname = async () => {
@@ -25,19 +28,41 @@ const NavBar = () => {
       <div className="flex gap-8 px-[24px] text-lg font-bold md:px-[40px] md:text-xl">
         내 대시보드
       </div>
-      <div className="flex">
-        <MembersImage />
-      </div>
-      <div className="flex gap-[16px]">
-        <Link href={'/mypage'} className="flex">
+      <div
+        onClick={toggleDropdown}
+        className="flex cursor-pointer items-center gap-[16px]"
+      >
+        <div className="pr-0 sm:pr-12">
           <ProfileImage />
-          <div className="mr-0 flex hidden self-center font-medium sm:block md:pr-40 lg:pr-80">
-            {nickname}
-          </div>
-        </Link>
+        </div>
+        <div className="mr-0 flex hidden self-center font-medium sm:block md:pr-40 lg:pr-80">
+          {nickname}
+        </div>
       </div>
+      {isDropdownOpen && (
+        <div className="absolute right-0 z-10 mt-125 w-100 rounded-lg border border-gray-200 bg-white shadow-lg md:mr-30 lg:mr-53">
+          <a
+            href="/"
+            className="align-center block flex justify-center px-4 py-2 text-gray-800 hover:bg-gray-100"
+          >
+            로그아웃
+          </a>
+          <a
+            href="/mypage"
+            className="align-center block flex justify-center px-4 py-2 text-gray-800 hover:bg-gray-100"
+          >
+            내 정보
+          </a>
+          <a
+            href="/mydashboard"
+            className="align-center block flex justify-center px-4 py-2 text-gray-800 hover:bg-gray-100"
+          >
+            내 대시보드
+          </a>
+        </div>
+      )}
     </div>
   );
 };
 
-export default NavBar;
+export default NavBarbasic;
