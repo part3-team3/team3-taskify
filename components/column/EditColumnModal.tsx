1f5ae45180a1ab0851c2ffe94071b6fb2af118bc
hoon <dudrn587@gmail.com>
2024-07-07 18:20:00 +0900

    Design: 대시보드 페이지 컬럼 수정 모달 반응형 디자인 적용

--- components/column/EditColumnModal.tsx
+++ components/column/EditColumnModal.tsx
@@ -1,5 +1,6 @@
 import Modal from '@/components/common/Modal';
 import useColumnTitleDuplicateChecker from '@/hooks/useColumnTitleDuplicateChecker';
+import useMediaQuery from '@/hooks/useMediaQuery';
 import { useState } from 'react';
 
 const EditColumnModal = ({
@@ -23,7 +24,8 @@ const EditColumnModal = ({
   const isTitleUpdated = columnTitle !== inputValue;
   const isDuplicate = useColumnTitleDuplicateChecker(dashboardId, inputValue);
 
-  const isCreateColumnModalDisabled = inputValue.trim() === '' ? true : false;
+  const isColumnEditModalDisabled = inputValue.trim() === '' ? true : false;
+  const isMobile = useMediaQuery('(max-width: 767px)');
 
   const openDeleteConfirmModal = () => setIsDeleteConfirmVisible(true);
 
@@ -43,65 +45,81 @@ const EditColumnModal = ({
       onClose={() => {
         setIsDeleteConfirmVisible(false);
       }}
-      width="540px"
-      height="276px"
+      width={isMobile ? '327px' : '540px'}
+      height={isMobile ? '241px' : '276px'}
     >
-      <h2 className="font-pretendard flex h-full flex-col items-center justify-center text-center font-medium text-[18]">
-        칼럼의 모든 카드가 삭제됩니다.
-      </h2>
-      <div className="mt-[-35px] flex justify-end gap-[12px]">
-        <button
-          className="btn_modal_large_white"
-          onClick={() => {
-            setIsDeleteConfirmVisible(false);
-          }}
-        >
-          취소
-        </button>
-        <button className="btn_modal_large_purple" onClick={handleDeleteClick}>
-          삭제
-        </button>
+      <div className="flex flex-col">
+        <div className="font-medium text-center mb-68 mt-53 text-16 md:mt-80">
+          칼럼의 모든 카드가 삭제됩니다.
+        </div>
+        <div className="flex gap-12 md:justify-end">
+          <button
+            className="btn_modal_small_white md:btn_modal_large_white"
+            onClick={() => {
+              setIsDeleteConfirmVisible(false);
+            }}
+          >
+            취소
+          </button>
+          <button
+            className="btn_modal_small_purple md:btn_modal_large_purple"
+            onClick={handleDeleteClick}
+          >
+            삭제
+          </button>
+        </div>
       </div>
     </Modal>
   ) : (
-    <Modal // Modal 컴포넌트에 넘겨주고 싶은 값을 prop으로 설정하기
+    <Modal
       isOpen={true}
       onClose={closeModal}
-      width="540px"
-      height="276px"
+      width={isMobile ? '327px' : '540px'}
+      height={isMobile ? '274px' : '276px'}
     >
-      <h2 className="mb-32 text-2xl font-bold">컬럼 관리</h2>
-      <p className="mb-10 h-21">이름</p>
-      <input
-        className={`mb-28 rounded p-16 border-1px-solid-gray-30 sm:h-[42px] sm:w-[287px] md:h-[48px] md:w-[484px] lg:h-[48px] lg:w-[484px] ${
-          isDuplicate ? 'border-red' : ''
-        }`}
-        value={inputValue}
-        placeholder="컬럼 제목을 입력해주세요"
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      {isTitleUpdated && isDuplicate && (
-        <p className="absolute mt-[-20px] text-[14px] text-red">
-          중복된 칼럼 이름입니다.
-        </p>
-      )}
-      <div className="flex">
-        <button
-          onClick={openDeleteConfirmModal}
-          className="flex items-end flex-grow underline text-gray-40 hover:text-violet-20"
-        >
-          삭제하기
-        </button>
-        <button onClick={closeModal} className="btn_modal_large_white">
-          취소
-        </button>
-        <button
-          disabled={isCreateColumnModalDisabled}
-          onClick={handleEditClick}
-          className={`ml-11 xl:ml-12 ${isCreateColumnModalDisabled ? 'btn_modal_large_gray' : 'btn_modal_large_purple'} `}
-        >
-          변경
-        </button>
+      <div className="flex flex-col gap-24 md:gap-32">
+        <h2 className="text-20 font-bold leading-[24px] text-black-20 md:text-24 md:leading-[29px]">
+          컬럼 관리
+        </h2>
+        <div className="flex flex-col gap-10">
+          <p className="h-21">이름</p>
+          <input
+            className={`h-42 w-287 rounded p-16 border-1px-solid-gray-30 md:h-48 md:w-484 ${
+              isDuplicate ? 'border-red' : ''
+            }`}
+            value={inputValue}
+            placeholder="컬럼 제목을 입력해주세요"
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+        </div>
+        {isTitleUpdated && isDuplicate && (
+          <p className="absolute mt-[-20px] text-[14px] text-red">
+            중복된 칼럼 이름입니다.
+          </p>
+        )}
+        <div className="flex flex-col gap-16 md:flex-row">
+          <button
+            onClick={openDeleteConfirmModal}
+            className="flex items-end flex-grow underline text-gray-40 hover:text-violet-20"
+          >
+            삭제하기
+          </button>
+          <div className="flex gap-12">
+            <button
+              onClick={closeModal}
+              className="btn_modal_small_white md:btn_modal_large_white"
+            >
+              취소
+            </button>
+            <button
+              disabled={isColumnEditModalDisabled}
+              onClick={handleEditClick}
+              className={` ${isColumnEditModalDisabled ? 'btn_modal_small_gray md:btn_modal_large_gray' : 'btn_modal_small_purple md:btn_modal_large_purple'} `}
+            >
+              변경
+            </button>
+          </div>
+        </div>
       </div>
     </Modal>
   );
